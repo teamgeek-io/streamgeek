885f4a8dd49de1c3c392f67e6757c7241ede34c7
Tom Radford <tom@theradford.com>
2025-08-07 16:52:57 +0200

    fix: typo

--- src/web/Document.tsx
+++ src/web/Document.tsx
@@ -8,7 +8,7 @@ export const Document: React.FC<{ children: React.ReactNode }> = ({
       <title>StreamGeek</title>
       <meta
         name="description"
-        content="Run your own video stream app on the cheap! 🚀🤓"
+        content="Run your own video streaming app on the cheap! 🚀🤓"
       />
       <link rel="modulepreload" href="/src/client.tsx" />
     </head>
