cdcf1bbd0f919a3e45ea0168a50d1297bf7a252d
Tom Radford <tom@theradford.com>
2025-08-08 11:01:02 +0200

    feat: video player

--- src/web/pages/video/id/video-player.tsx
+++ src/web/pages/video/id/video-player.tsx
@@ -2,17 +2,47 @@
 
 import HlsVideoElement from "hls-video-element/react";
 import { Video } from "../../../../db";
+import {
+  MediaControlBar,
+  MediaController,
+  MediaFullscreenButton,
+  MediaLoadingIndicator,
+  MediaMuteButton,
+  MediaPlaybackRateButton,
+  MediaPlayButton,
+  MediaSeekBackwardButton,
+  MediaSeekForwardButton,
+  MediaTimeDisplay,
+  MediaTimeRange,
+  MediaVolumeRange,
+} from "media-chrome/react";
 
 export const VideoPlayer = ({ video }: { video: Video }) => {
   return (
-    <HlsVideoElement
-      src={video.playlistUrl!}
-      // className="absolute top-0 right-0 left-0"
-      // slot="media"
-      // suppressHydrationWarning
-      autoplay
-      muted
-      loop
-    />
+    <MediaController>
+      <HlsVideoElement
+        src={video.playlistUrl!}
+        // className="absolute top-0 right-0 left-0"
+        slot="media"
+        preload="auto"
+        suppressHydrationWarning
+      />
+      <MediaControlBar>
+        <MediaPlayButton></MediaPlayButton>
+        <MediaSeekBackwardButton></MediaSeekBackwardButton>
+        <MediaSeekForwardButton></MediaSeekForwardButton>
+        <MediaTimeRange></MediaTimeRange>
+        <MediaTimeDisplay showDuration remaining></MediaTimeDisplay>
+        <MediaMuteButton></MediaMuteButton>
+        <MediaVolumeRange></MediaVolumeRange>
+
+        <MediaFullscreenButton></MediaFullscreenButton>
+        <MediaPlaybackRateButton></MediaPlaybackRateButton>
+      </MediaControlBar>
+      <MediaLoadingIndicator
+        slot="centered-chrome"
+        noAutohide
+      ></MediaLoadingIndicator>
+    </MediaController>
   );
 };
