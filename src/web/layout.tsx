f8d42d5e4668c7c603d2b81dea0585ab30d3f830
Tom Radford <tom@theradford.com>
2025-08-11 21:50:11 +0200

    feat: dark mode provider

--- src/web/layout.tsx
+++ src/web/layout.tsx
@@ -1,10 +1,15 @@
+"use client";
+
 import { LayoutProps } from "rwsdk/router";
+import { ThemeProvider } from "@/web/components/theme-provider";
+import { ModeToggle } from "@/web/components/mode-toggle";
 
 export function AppLayout({ children }: LayoutProps) {
   return (
-    <div>
+    <ThemeProvider defaultTheme="dark" storageKey="theme">
       <h1>StreamGeek</h1>
+      <ModeToggle />
       {children}
-    </div>
+    </ThemeProvider>
   );
 }
