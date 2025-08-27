856b957d3918b7e74db2e298f573941fe54a2953
Tom Radford <tom@theradford.com>
2025-08-27 17:02:40 +0200

    fix: headers for image

--- src/web/headers.ts
+++ src/web/headers.ts
@@ -26,6 +26,6 @@ export const setCommonHeaders =
     // Defines trusted sources for content loading and script execution:
     response.headers.set(
       "Content-Security-Policy",
-      `default-src 'self'; script-src 'self' 'nonce-${nonce}' https://challenges.cloudflare.com; style-src 'self' 'unsafe-inline'; frame-src https://challenges.cloudflare.com; connect-src 'self' *; media-src 'self' blob: data:; object-src 'none';`
+      `default-src 'self'; script-src 'self' 'nonce-${nonce}' https://challenges.cloudflare.com; style-src 'self' 'unsafe-inline'; img-src 'self' data: blob: *; frame-src https://challenges.cloudflare.com; connect-src 'self' *; media-src 'self' blob: data:; object-src 'none';`
     );
   };
