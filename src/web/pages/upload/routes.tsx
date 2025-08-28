e32c7cab840fdacf14abe2fc8b6ed32725967419
Tom Radford <tom@theradford.com>
2025-08-28 10:05:39 +0200

    fix: auth middleware instead of interceptor

--- src/web/pages/upload/routes.tsx
+++ src/web/pages/upload/routes.tsx
@@ -6,38 +6,42 @@ import { db } from "../../../db";
 import { link } from "../../shared/links";
 import { AppContext } from "../../../worker";
 
-export const uploadRoutes = [
-  async ({ ctx }: { ctx: AppContext }) => {
-    if (!ctx.user) {
-      return new Response(null, {
-        status: 302,
-        headers: { Location: "/user/login" },
-      });
-    }
-  },
-  route("/", () => <CreateUpload />),
-  route("/:id", async ({ params }) => {
-    const { id } = params;
-
-    const doneJob = await db.job.findFirst({
-      where: {
-        videoId: id,
-        status: {
-          in: ["done"],
-        },
-      },
+const requireAuth = async ({ ctx }: { ctx: AppContext }) => {
+  if (!ctx.user) {
+    return new Response(null, {
+      status: 302,
+      headers: { Location: link("/user/login") },
     });
+  }
+};
 
-    // Redirect early if there's already a video that's uploaded
-    if (doneJob) {
-      return new Response(null, {
-        status: 302,
-        headers: {
-          Location: link("/video/:id", { id: doneJob.videoId }),
+export const uploadRoutes = [
+  route("/", [requireAuth, CreateUpload]),
+  route("/:id", [
+    requireAuth,
+    async ({ params }) => {
+      const { id } = params;
+
+      const doneJob = await db.job.findFirst({
+        where: {
+          videoId: id,
+          status: {
+            in: ["done"],
+          },
         },
       });
-    }
 
-    return <UploadEditorPage params={params} />;
-  }),
+      // Redirect early if there's already a video that's uploaded
+      if (doneJob) {
+        return new Response(null, {
+          status: 302,
+          headers: {
+            Location: link("/video/:id", { id: doneJob.videoId }),
+          },
+        });
+      }
+
+      return <UploadEditorPage params={params} />;
+    },
+  ]),
 ];
