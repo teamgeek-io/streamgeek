37fdd81083e9b8b28db1829b4bbfe5659558f052
Tom Radford <tom@teamgeek.io>
2025-08-29 16:58:32 +0200

    wip

--- src/web/pages/video/id/index.tsx
+++ src/web/pages/video/id/index.tsx
@@ -15,6 +15,8 @@ export async function VideoPage({ ctx, params }: RequestInfo) {
     return <div>Video not found</div>;
   }
 
+  const isVertical = video.width && video.height && video.width < video.height;
+
   return (
     <>
       <title>{`${video.title} - StreamGeek`}</title>
@@ -25,12 +27,9 @@ export async function VideoPage({ ctx, params }: RequestInfo) {
             video={video}
             className={cn(
               "w-full ",
-              video.width && video.height
-                ? video.width > video.height
-                  ? "aspect-16/9 max-h-[75dvh]"
-                  : //wip
-                    "aspect-9/16 max-h-full max-w-[50%]"
-                : "aspect-16/9 max-h-[75dvh]"
+              !isVertical
+                ? "aspect-16/9 max-h-[75dvh]"
+                : "aspect-9/16 max-h-full max-w-[50dvw]"
             )}
           />
         )}
